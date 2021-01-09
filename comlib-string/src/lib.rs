5a108c19385a84768aa0c689ead9ec8fd72378a6
Henrik Lievonen <henrik.lievonen@hotmail.com>
2021-01-09 18:22:02 +0200

    Document string library

--- comlib-string/src/lib.rs
+++ comlib-string/src/lib.rs
@@ -1,2 +1,15 @@
+//! # Comlib String Utilities
+//! This library contains some commonly used string algorithms.
+//!
+//! ## Content
+//! - [Rolling hash](RollingHash)
+//!
+//! ## Still missing
+//! - Z algorithm
+//! - Automata
+//! - Pattern matching
+
+#![warn(missing_docs)]
+
 mod rolling_hash;
 pub use rolling_hash::RollingHash;
